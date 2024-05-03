4359e167c3f9862d3a8a5c9f2eb38fb063ed65b7
LeticiaMacFre <leticia.macedo2705@gmail.com>
2024-05-03 17:24:09 -0300

    commit tentativa pulo

--- Assets/Scripts/robsonAndando.cs
+++ Assets/Scripts/robsonAndando.cs
@@ -8,6 +8,8 @@ public class robsonAndando : MonoBehaviour
     private Rigidbody2D rigAndando;
     public Animator animPlayer;
     private float scaleX;
+    public float JumpForce;
+    public bool isJumping;
     // Start is called before the first frame update
     void Start()
     {
@@ -38,10 +40,20 @@ public class robsonAndando : MonoBehaviour
         {
             animPlayer.SetLayerWeight(1, 0);
         }
+        if(Input.GetKey(KeyCode.Space) && !isJumping)
+        {
+            animPlayer.SetLayerWeight(3, 1);
+            Pulando();
+        }
+        else
+        {
+            animPlayer.SetLayerWeight(3, 0);
+        }
     }
     void FixedUpdate()
     {
         MoveAndando();
+        Pulando();
     }
 
     private void MoveAndando()
@@ -49,6 +61,22 @@ public class robsonAndando : MonoBehaviour
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * speedAndando;
     }
+
+    private void Pulando()
+    {
+        rigAndando.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+        isJumping = true;
+    }
+
+    void OnCollisionEnter2D(Collision2D collisor)
+    {
+        if (collisor.gameObject.CompareTag("ground"))
+        {
+            isJumping = false;
+        }
+    }
+
+
 
   
 
