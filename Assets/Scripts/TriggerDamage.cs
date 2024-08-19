e402075dec7447ebef77bdccf3747e5e3871a60f
zioARNT <136367081+zioARNT@users.noreply.github.com>
2024-08-19 14:26:57 -0300

    dano e vida

---
+++ Assets/Scripts/TriggerDamage.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDamage : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+
+}
