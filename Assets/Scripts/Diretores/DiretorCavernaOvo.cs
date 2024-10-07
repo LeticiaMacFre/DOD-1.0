1438a4b4d6f0a16fe6084568193c4831c4fabac8
LeticiaMacFre <leticia.macedo2705@gmail.com>
2024-10-07 17:16:06 -0300

    dialogo caverna

--- Assets/Scripts/Diretores/DiretorCavernaOvo.cs
+++ Assets/Scripts/Diretores/DiretorCavernaOvo.cs
@@ -6,23 +6,31 @@ public class DiretorCavernaOvo : MonoBehaviour
 {
     public GameObject ovo;
     private Animator ovoAnim;
+    
     public GameObject dragao;
+    
     public robsonAndando robson;
     public Animator robsonAnim;
+    
     public GameObject saida;
     public GameObject sound;
+    
     public AudioSource audioSource;
     public AudioClip openEgg;
     public AudioClip caveLoop;
+    
     public bool startAnimation = false;
     public float tempoCena = 7.0f;
     private bool cenaFulga;
+
+    private SistemaDeDialogo sysDialogo;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(FadeInicial());
         robson = GameObject.FindGameObjectWithTag("Player").GetComponent<robsonAndando>();
+        sysDialogo = GameObject.FindGameObjectWithTag("Dialogo").GetComponent<SistemaDeDialogo>();
         robsonAnim = robson.GetComponent<Animator>();
         ovoAnim = ovo.GetComponent<Animator>();
         audioSource = sound.GetComponent<AudioSource>();
